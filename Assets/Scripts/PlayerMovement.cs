6ff67a962bddffa41b59afad3e624ca59518d402
Vahn <vahnkess@gmail.com>
2023-09-26 13:02:16 +0200

    Testing Scene Updates
    Created animations and added them to the testing scene. Still working on coordinating them with player movement.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,7 @@ public class PlayerMovement : MonoBehaviour
     private float raycastLength = 0.85f;
     public LayerMask platformLayerMask;
     private bool isGrounded;
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
