3ccfcaa5add2e382033aa26c6cfb2438438d4c37
neel <troegern@whitman.edu>
2023-10-03 16:27:30 +0200

    s

--- Assets/Scripts/PlayerInstrument.cs
+++ Assets/Scripts/PlayerInstrument.cs
@@ -10,7 +10,7 @@ public class PlayerInstrument : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // starting instrument is currently piano
+        // default starting instrument is currently piano
         currentInstrument = Instrument.Piano;
         inventory[0] = Instrument.Piano;
     }
@@ -18,14 +18,14 @@ public class PlayerInstrument : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       // if player picks up new isntrument: add to list,
-       // if player swaps instrument, change currentInstrument
+       /*
+       if player swaps instrument, change currentInstrument
 
-        //inventory[0] is piano     (1)
-        //inventory[1] is guitar    (2)
-        //inventory[2] is flute     (3)
-        //inventory[3] is drums     (4)
-
+       inventory[0] is piano     (1)
+       inventory[1] is guitar    (2)
+       inventory[2] is flute     (3)
+       inventory[3] is drums     (4)
+        */
        if (Input.GetKeyDown(KeyCode.Alpha1) && inventory[0] != null)
        {
             currentInstrument = inventory[0];
@@ -53,28 +53,29 @@ public class PlayerInstrument : MonoBehaviour
         return currentInstrument;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (collision.CompareTag("instrument")) //this can be changed later since comparing with tags is terrible
+        if (other.CompareTag("instrument")) //this can be changed later since comparing with tags is terrible
         {
-            pickingUp = true;
-            if (collision.name == "piano")
+            pickingUp = true; // to be used to freeze playermovement in PlayerMovement
+            // including the piano case if we want to change the default instrument
+            if (other.name == "piano")
             {
                 inventory[0] = Instrument.Piano;
             }
-            if (collision.name == "guitar")
+            if (other.name == "guitar")
             {
                 inventory[1] = Instrument.Guitar;
             }
-            if (collision.name == "flute")
+            if (other.name == "flute")
             {
                 inventory[2] = Instrument.Flute;
             }
-            if (collision.name == "drums")
+            if (other.name == "drums")
             {
                 inventory[3] = Instrument.Drums;
             }
-            Destroy(collision.gameObject);
+            Destroy(other.gameObject); //if we want juice we can make an animation for picking up the 
         }
     }
 
