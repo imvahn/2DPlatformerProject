9d0fc641a4bec40702a3ccbbcdab8c88e64ce67d
neel <troegern@whitman.edu>
2023-10-06 13:29:22 +0200

    d

--- Assets/Scripts/PlayerInstrument.cs
+++ Assets/Scripts/PlayerInstrument.cs
@@ -18,44 +18,8 @@ public class PlayerInstrument : MonoBehaviour
         inventory[1] = false;
         inventory[2] = false;
         inventory[3] = false;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        /*
-        if player swaps instrument, change currentInstrument
-
-        inventory[0] is piano     (1)
-        inventory[1] is guitar    (2)
-        inventory[2] is flute     (3)
-        inventory[3] is drums     (4)
-         */
-        if (Input.GetKeyDown(KeyCode.Alpha1) && inventory[0] == true)
-        {
-            currentInstrument = Instrument.Piano;
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha2) && inventory[1] == true)
-        {
-            currentInstrument = Instrument.Guitar;
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha3) && inventory[2] == true)
-        {
-            currentInstrument = Instrument.Flute;
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha4) && inventory[3] == true)
-        {
-            currentInstrument = Instrument.Drums;
-        }
-    }
-
-    public static bool[] GetInventory()
-    {
-        return inventory;
-    }
-    public static string GetInstrument()
-    {
-        return currentInstrument;
+        //check if inventory full using inventory == [true, true, true, true]
     }
 
     private void OnTriggerEnter2D(Collider2D other)
