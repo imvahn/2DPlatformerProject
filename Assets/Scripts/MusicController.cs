f395079086bdd20b632f4e7b636ef45cb95e0d82
CassidyOlson <cassidy.m.olson@gmail.com>
2023-10-07 17:59:40 +0200

    Music Controller fixed

--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -18,6 +18,8 @@ public class MusicController : MonoBehaviour
 
     public AudioSource musicSource;
 
+    private int count;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,13 +28,34 @@ public class MusicController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (PlayerMovement.swapped == true)
+        if (PlayerMovement.swapped == true && count == 0)
         {
+            if (count == 0)
+            {
+                StopMusic();
+                count = 1;
+            }
             // Check if music is currently playing and the instrument has changed
-            if (PlayerInstrument.currentInstrument == Instrument.Piano) { PlayMusic(pianoMusic, pianoTransition); }
-            else if (PlayerInstrument.currentInstrument == Instrument.Flute) { PlayMusic(fluteMusic, fluteTransition); }
-            else if (PlayerInstrument.currentInstrument == Instrument.Drums) { PlayMusic(drumMusic, drumTransition); }
-            else if (PlayerInstrument.currentInstrument == Instrument.Guitar) { PlayMusic(guitarMusic, guitarTransition); }
+            if (PlayerInstrument.currentInstrument == Instrument.Piano) 
+            { 
+                PlayMusic(pianoMusic, pianoTransition); 
+            }
+            else if (PlayerInstrument.currentInstrument == Instrument.Flute) 
+            { 
+                PlayMusic(fluteMusic, fluteTransition); 
+            }
+            else if (PlayerInstrument.currentInstrument == Instrument.Drums) 
+            { 
+                PlayMusic(drumMusic, drumTransition); 
+            }
+            else if (PlayerInstrument.currentInstrument == Instrument.Guitar) 
+            { 
+                PlayMusic(guitarMusic, guitarTransition); 
+            }
+        }
+        if (PlayerMovement.swapped == false)
+        {
+            count = 0;
         }
     }
 
